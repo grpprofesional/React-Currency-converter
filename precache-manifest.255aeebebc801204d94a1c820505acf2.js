47c8b23bd241345417a292b86ec71a811c807e97
grpprofesional <bonannoangelo0307@gmail.com>
2019-07-29 23:48:56 +0100

    Updates

---
+++ precache-manifest.255aeebebc801204d94a1c820505acf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d006d1f0f398d197f409233d961b55eb",
+    "url": "/React-Currency-converter/index.html"
+  },
+  {
+    "revision": "f5df3e7c02abd2afd2f3",
+    "url": "/React-Currency-converter/static/css/main.e31ea872.chunk.css"
+  },
+  {
+    "revision": "f3e66963fb04d329ad9f",
+    "url": "/React-Currency-converter/static/js/2.f2dd8a08.chunk.js"
+  },
+  {
+    "revision": "f5df3e7c02abd2afd2f3",
+    "url": "/React-Currency-converter/static/js/main.550c9f59.chunk.js"
+  },
+  {
+    "revision": "def9c0af60647e48bcbf",
+    "url": "/React-Currency-converter/static/js/runtime~main.ece9153c.js"
+  }
+]);
